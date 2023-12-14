1718a1f53ef3d57c217b39953d87fbe90c819ef4
Diego <dvelarde140@gmail.com>
2023-12-14 17:17:06 -0500

    REGISTRO

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+
+
+    name : ['', [Validators.required]],
+    email : ['', [Validators.required]],
+    username : ['', [Validators.required]],
+    password : ['', [Validators.required, Validators.minLength(6)]],
+    password2 : ['', [Validators.required]],
+
+  });
+  constructor(private fb: FormBuilder) { }
+
+
+  
+  onSubmit(){
+  
+   this.myForm.markAllAsTouched();
+
+  }
+  isValidField(field: string){
+
+    //obtener la validacion desde un servicio
+
+  }
 }
